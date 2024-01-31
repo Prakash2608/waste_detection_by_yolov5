a676465fca5f47a59359fafe1372b00f4d58e49d
Prakash2608 <prakashraj822@gmail.com>
2024-01-31 11:41:55 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author = 'Prakash',
+    author_email = 'prakashraj822@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
